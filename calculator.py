6c1046898232ee85bacc87755a2596f746302d33
Tanvi Seelam <tanvi@raritop.local>
2025-02-15 10:51:35 -0500

    Initial commit with add function

---
+++ calculator.py
@@ -0,0 +1,17 @@
+def add(a,b):
+    return a+b
+
+number1 = int(input("Enter First number: "))
+number2 = int(input("Enter second number: "))
+operator = input("Enter +,-,*,/: ")
+if (operator == "+"):
+    print (add(number1,number2))
+elif (operator == "-"):
+    print()
+elif (operator == "*"):
+    print()
+elif (operator == "/"):
+    print()
+else:
+    print("Unknown Operator")
+    
