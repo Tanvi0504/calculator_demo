d2b49fd20a64d1dad64dbf67dd23e5a4fd300783
Madhu <programmingwithkotumadhupriya@gmail.com>
2025-02-15 21:50:56 +0530

    subtract function added

--- calculator.py
+++ calculator.py
@@ -5,13 +5,16 @@
 def add(a,b):
     return a+b
 
+def sub(a,b):
+    return a-b
+
 number1 = int(input("Enter First number: "))
 number2 = int(input("Enter second number: "))
 operator = input("Enter +,-,*,/: ")
 if (operator == "+"):
     print (add(number1,number2))
 elif (operator == "-"):
-    print()
+    print(sub(number1,number2))
 elif (operator == "*"):
     print()
 elif (operator == "/"):
